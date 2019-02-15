b11ed094dc98ba2f3e0d7e31803ac70ec574360d
K <kiyunkim@users.noreply.github.com>
2019-02-15 16:51:07 -0500

    jquery info was incorrect

--- javascript/ajax,json/exercise/example.js
+++ javascript/ajax,json/exercise/example.js
@@ -44,16 +44,3 @@ xhr.onload = function() {
 
 // send method - 'activates' the open
 xhr.send();
-
-
-
-// above in jquery:
-$.ajax({
-  type: 'GET',
-  url: 'data.json',
-  async: true,
-  dataType: 'json',
-  success: function(data){
-    console.log(data);
-  }
- });
